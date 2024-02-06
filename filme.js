3e4499916df874707529369c75a97876feac8941
leticialarruda <leticia.pireslsouza@gmail.com>
2024-02-06 14:49:09 -0300

    Atualização do códdigo JS, adição de filme na lista

--- filme.js
+++ filme.js
@@ -1,41 +1,78 @@
-const filmes = [];
+const filmes = []
+
+function preencherDropdown() {
+  const listaOpcoes = document.querySelector('.form_opcao');
+
+  const opcoesGenero = ['ação','animação','Aventura','Comédia','Documentário','Drama','Fantasia','Ficção Científica','Guerra','Romance','Suspense','Terror'];
+
+  opcoesGenero.forEach(function(opcao) {
+    var opcFilme = document.createElement('option')
+    opcFilme.innerText = opcao
+    opcFilme.value = opcao
+
+    listaOpcoes.appendChild(opcFilme);
+  })
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  preencherDropdown();
+
+  var listaFilmesContainer = document.querySelector('.lista');
+  
+  
+  var listaFilmes = JSON.parse(localStorage.getItem('filmes')) || [];
+
+  listaFilmes.forEach(function(filme) {
+    var divFilme = document.createElement('div');
+    divFilme.classList.add('lista_filmes');
+
+    divFilme.innerHTML = `
+      <div class="lista_filmes_um"></div>
+      <img class="img_poster" src="${filme.imagem}" />
+      <div class="text_container">
+        <p class="lista_titulos">${filme.titulo}</p>
+        <p class="lista_titulos_p">${filme.genero}</p>
+        <p class="lista_titulos_p">${filme.sinopse}</p>
+        <img class="icon_fav" src="/Imagens/icon_fav.png" />
+      </div>
+    `;
+
+    listaFilmesContainer.appendChild(divFilme);
+  });
+  
+});
+
 
 function salvar() {
+  
   const titulo = document.getElementById("titulo").value;
   const genero = document.getElementById("genero").value;
   const sinopse = document.getElementById("sinopse").value;
-  const imagem = document.getElementById("modal_file").value;
+  const inputImagem = document.getElementById('modal_file');
+  const imagemFile = inputImagem.files[0];
+  
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    const imagemBase = e.target.result;
+ 
 
   const filme = {
     titulo: titulo,
     genero: genero,
     sinopse: sinopse,
-    imagem: imagem,
+    imagem: imagemBase,
   };
 
-  filmes.push(filme);
+  filmes.push(filme)
 
-  localStorage.setItem("filmes", JSON.stringify(filmes));
+  localStorage.setItem("filmes", JSON.stringify(filmes))
+
+  document.getElementById("titulo").value = "";
+    document.getElementById("genero").value = "";
+    document.getElementById("sinopse").value = "";
+    inputImagem.value = "";
+  }
+  reader.readAsDataURL(new Blob([imagemFile]));
 }
-
-function listar() {
-  const filmesSalvos = JSON.parse(localStorage.getItem("filmes"));
-  
-  const listaHTML = filmesSalvos.map((element) => {
-    return `
-      <div class="lista_filmes">
-        <div class="lista_filmes_um"></div>
-        <img class="img_poster" src="${element.modal_file}" />
-        <div class="text_container">
-          <p class="lista_titulos">${element.titulo}</p>
-          <p class="lista_titulos_p">${element.sinopse}</p>
-          <img class="icon_fav" src="/Imagens/icon_fa v.png" />
-        </div>
-      </div>
-    `;
-  });
-
-  document.querySelector(".lista").innerHTML = listaHTML.join("");
-  console.log(filmesSalvos);
-}
-
